74f516fca5bc0cd0afcf14cba5559653353ebd14
Up & Daun <82289435+updaun@users.noreply.github.com>
2021-08-12 14:23:57 +0900

    file size show and progress bar test

--- src/youtube_download.py
+++ src/youtube_download.py
@@ -1,6 +1,10 @@
 import os
 from tkinter import messagebox
 from pytube import YouTube
+import sys
+
+import pytube.request
+pytube.request.default_range_size = 9437184
 
 class Downloader:
         
@@ -36,17 +40,48 @@ class Downloader:
             print('[에러내용]', all_e,"\n")
             messagebox.showwarning("유튜브 다운로드 프로그램", "[오류] 폴더 생성을 실패하였습니다.")
 
+
+    def show_progress_bar(stream, _chunk, _file_handle, bytes_remaining):
+        current = ((stream.filesize - bytes_remaining)/stream.filesize)
+        percent = ('{0:.1f}').format(current*100)
+        progress = int(50*current)
+        status = '█' * progress + '-' * (50 - progress)
+        sys.stdout.write(' ↳ |{bar}| {percent}%\r'.format(bar=status, percent=percent))
+        sys.stdout.flush()
+
     # 유튜브 동영상 다운로드 함수 
     def youtube_download(self, youtube_url):
-            try:
-                # 동영상 다운로드 메서드
-                YouTube(youtube_url).streams.filter(res="720p").first().download('./Downloads/')
-                print('[완료] 동영상을 성공적으로 다운로드 되었습니다.\n')
-                messagebox.showinfo(
-                "유튜브 다운로드 프로그램", "동영상이 성공적으로 다운로드 되었습니다.")
-            except Exception as all_e:
-                print('[오류] 링크 주소를 다시 확인해 주세요.\n')
-                print('[에러코드]', type(all_e))
-                print('[에러내용]', all_e,"\n")
-                messagebox.showinfo(
-                "유튜브 다운로드 프로그램", "[오류] 링크 주소를 다시 확인해 주세요.")
+
+        global file_size
+
+        print('[진행] 동영상 다운로드를 시작합니다.\n')
+
+        try:
+            # 동영상 다운로드 메서드
+            video = YouTube(youtube_url, on_progress_callback=self.show_progress_bar)
+            video.register_on_progress_callback(self.show_progress_bar)
+            video_type = video.streams.filter(progressive = True, file_extension = "mp4").first()
+            video_type.download('./Downloads/')
+        
+        except Exception as all_e:
+            print('[오류] 링크 주소를 다시 확인해 주세요.\n')
+            print('[에러코드]', type(all_e))
+            print('[에러내용]', all_e,"\n")
+            messagebox.showinfo(
+            "유튜브 다운로드 프로그램", "[오류] 링크 주소를 다시 확인해 주세요.")
+
+        file_size = video_type.filesize
+        print("동영상 용량 :", round(file_size/1000000, 2),"MB\n")
+
+        print('[완료] 동영상을 성공적으로 다운로드 되었습니다.\n')
+        messagebox.showinfo(
+        "유튜브 다운로드 프로그램", "동영상이 성공적으로 다운로드 되었습니다.")
+
+# test code
+with open('./test_url.txt', 'r') as file_data:
+    for line in file_data:
+        print(line)
+        test_url = line
+        
+dl = Downloader()
+dl.youtube_download(test_url)
